91e9c7945bbf3c82c69bd20b0a52091d41daf6b7
1Tech-X <30934772+1Tech-X@users.noreply.github.com>
2020-07-27 07:37:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -128,30 +128,6 @@ xorg_twm () {
 	fi
 }
 
-#VNC server xstartup configuration for xfce4
-
-xfce_conf () {
-
-	echo""
-	echo "${red}[>]${cyan}configuring vnc xstartup....${reset}\n"
-	sleep 1
-	echo "#!/data/data/com.termux/files/usr/bin/sh" > .vnc/xstartup
-	echo "## This file is execute during VNC server" >> .vnc/xstartup
-	echo "## startup" >> .vnc/xstartup
-	echo "" >> .vnc/xstartup
-	echo "# Launch terminal emulator Aterm" >> .vnc/xstartup
-	echo "# Required package 'aterm'" >> .vnc/xstartup
-	echo "aterm -geometry 80x24+10+10 -ls &" >> .vnc/xstartup
-	echo "" >> .vnc/xstartup
-	echo "# Launch Tab Window manager " >> .vnc/xstartup
-	echo "# Required package 'xorg-twm'" >> .vnc/xstartup
-	echo "twm &" >> .vnc/xstartup
-	echo "xfce4-session &" >> .vnc/xstartup
-	echo ""
-	echo "${green}[*]${blue}Done${reset}\n"
-	echo ""
-}
-
 #Extra package for xfce4-desktop
 
 netsurf () {
